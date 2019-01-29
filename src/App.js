2ed88ea1a01d9b66ecbd31605e7eb25c3445156f
behnam rostami <behnam.rostamy.br@gmail.com>
2019-01-29 11:41:38 +0330

    natour: added header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import './App.scss';
+
+import Home from "./views/home";
+import Natour from "./views/natour";
 
 class App extends Component {
+  state = {
+    show: true
+  }
+  toggleShow = () => {
+    this.setState({show: false});
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/" exact={true} component={Home}/>
+          <Route path="/natour" component={Natour}/>
+          {this.state.show ? (
+            <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link onClick={this.toggleShow} to="/natour">Natour</Link></li>
+          </ul>
+          ) : null}
+
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/views/natour.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AppHeader from "../components/natour/AppHeader";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div class="natour">
+        <AppHeader />
+      </div>
+    );
+  }
+} 
